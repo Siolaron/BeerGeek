04a60597b6215b366eca0b4f86c97e94194cbf7f
Bryan Neveu <bryan@pando-studio.com>
2022-05-11 23:31:56 +0200

    FEAT : Home page

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,7 +1,11 @@
 <nav class="mainnavbar">
-    <ul class="mainnavbar__list">
-        <li class="mainnavbar__item"><a href="../">Accueil</a></li>
-        <li class="mainnavbar__item"><a href="../inventaire.php">Inventaire</a></li>
-        <li class="mainnavbar__item"><a href="../creer-une-recette.php">Créer une recette</a></li>
-    </ul>
+    <a href="../" class="mainnavbar__title ff--nuxt fs--xl">
+        <h2>BeerGeek.</h2>
+    </a>
+    <div class="mainnavbar__content--center">
+        <ul class="mainnavbar__list">
+            <li class="mainnavbar__item fw--semibold"><a href="../inventaire.php" class="underlined">Inventaire</a></li>
+            <li class="mainnavbar__item fw--semibold"><a href="../creer-une-recette.php" class="underlined">Créer une recette</a></li>
+        </ul>
+    </div>
 </nav>
--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php require_once('./inc/head.php') ?>
 
 <main>
-    <h1>Accueil</h1>
+    <section class="home__container">
+        <h2 class="home__title ff--nuxt">Make beer</h2>
+        <!-- <img src="./assets//img/beer.png" alt=""> -->
+        <img src="./assets//img/home__beer2.png" alt="beer illustration" class="home__beer">
+        <!-- <img src="./assets//img/makebeer.png" alt=""> -->
+        <h2 class="home__title ff--nuxt">great again</h2>
+    </section>
 </main>
 
 <?php require_once('./inc/footer.php') ?>
